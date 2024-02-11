99590d93f012eb151afd1cf698bcb90616de92e8
Zoe Matrullo <104142441+zoematr@users.noreply.github.com>
2024-02-11 16:54:24 +0100

    got human sentences
    got human sentences and cleaned them

---
+++ data/cleanlines.py
@@ -0,0 +1,7 @@
+with open("/Users/zoe/Documents/GitHub/zoematr/Reverse_Transformer/data/HU_sentences.txt", "r") as input_file, open("/Users/zoe/Documents/GitHub/zoematr/Reverse_Transformer/data/clean_sentences.txt", "w") as output_file:
+    # Iterate over each line in the input file
+    for line in input_file:
+        # Remove unwanted characters (numbers, commas, and colons)
+        cleaned_line = ''.join(char for char in line if char.isalpha() or char.isspace())
+        # Write the cleaned line to the output file
+        output_file.write(cleaned_line)
---
+++ data/get_human_sentences.py
@@ -0,0 +1,28 @@
+import nltk
+import random
+
+# Download the Gutenberg corpus if not already downloaded
+nltk.download('gutenberg')
+nltk.download('punkt')
+
+# Load Gutenberg corpus
+from nltk.corpus import gutenberg
+
+
+# Get a list of sentences from Gutenberg corpus
+sentences = gutenberg.sents()
+
+# Filter sentences based on length
+filtered_sentences = [sent for sent in sentences if 20 <= len(sent) <= 40]
+
+# Shuffle the sentences
+random.shuffle(filtered_sentences)
+
+# Select 400 sentences (or less if there aren't enough available)
+selected_sentences = filtered_sentences[:400]
+
+# Write selected sentences to a text file
+with open("/Users/zoe/Documents/GitHub/zoematr/Reverse_Transformer/data/HU_sentences.txt", "w") as file:
+    for sent in selected_sentences:
+        # Join words in the sentence and write to the file
+        file.write(" ".join(sent) + "\n")
